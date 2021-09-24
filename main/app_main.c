81ea46b14ae47b3ab9c2e933a12a75019df38701
relyq <nicoale_mainenti@hotmail.es>
2021-09-24 08:12:26 -0300

    refactoring

--- main/app_main.c
+++ main/app_main.c
@@ -7,6 +7,8 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 
+#include "app_main.h"
+
 #include <inttypes.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -16,7 +18,6 @@
 #include <sys/param.h>
 
 #include "cJSON.h"
-#include "dht.h"
 #include "driver/gpio.h"
 #include "esp32/rom/ets_sys.h"
 #include "esp_event.h"
@@ -34,18 +35,13 @@
 #include "lwip/netdb.h"
 #include "lwip/sockets.h"
 #include "mqtt_client.h"
+#include "mqttpoint/mqttpoint.h"
 #include "nvs_flash.h"
 #include "protocol_examples_common.h"
 #include "sdkconfig.h"
+#include "sensors/sensors.h"
 
 static const char *TAG = "MQTT_POINT";
-
-struct sensor_msg {
-  char DeviceClass[5];
-  char IdDevice[9];
-  char MsgType[5];
-  char MsgContent[64];
-};
 
 QueueHandle_t xMQTTDHTQueue;
 
@@ -59,75 +55,6 @@ extern const uint8_t mqtt_eclipse_org_pem_start[] asm(
 #endif
 extern const uint8_t mqtt_eclipse_org_pem_end[] asm(
     "_binary_mqtt_eclipse_org_pem_end");
-
-void DHT_task(void *pvParameter) {
-  vTaskDelay(5000 / portTICK_PERIOD_MS);
-
-  printf("Starting DHT Task\n\n");
-
-  static const dht_sensor_type_t sensor_type = DHT_TYPE_DHT11;
-  static const gpio_num_t dht_gpio = 4;
-  struct sensor_msg DHT_1;
-  strcpy(DHT_1.DeviceClass, "08");
-  strcpy(DHT_1.IdDevice, "000D2BF9");
-  strcpy(DHT_1.MsgType, "02");
-
-  while (1) {
-    float temp;
-    float hum;
-    if (dht_read_float_data(sensor_type, dht_gpio, &hum, &temp) == ESP_OK) {
-      printf("DHT_1: temp: %.2fC humidity: %.2f\n", temp, hum);
-      sprintf(DHT_1.MsgContent, "T%05.2fH%02.2f", temp, hum);
-      xQueueSendToBack(xMQTTDHTQueue, &DHT_1, 0);
-    } else {
-      printf("DHT_1: could not read data from sensor\n");
-    }
-
-    vTaskDelay(2000 / portTICK_PERIOD_MS);
-  }
-}
-
-void DHT_test0(void *pvParameter) {
-  vTaskDelay(5000 / portTICK_PERIOD_MS);
-
-  printf("Starting DHT Task\n\n");
-
-  struct sensor_msg DHT_2;
-  strcpy(DHT_2.DeviceClass, "08");
-  strcpy(DHT_2.IdDevice, "523G9VK5");
-  strcpy(DHT_2.MsgType, "02");
-
-  while (1) {
-    float temp = 13.3;
-    float hum = 20;
-    printf("DHT_2: temp: %.2fC humidity: %.2f\n", temp, hum);
-    sprintf(DHT_2.MsgContent, "T%05.2fH%02.2f", temp, hum);
-    xQueueSendToBack(xMQTTDHTQueue, &DHT_2, 0);
-
-    vTaskDelay(2000 / portTICK_PERIOD_MS);
-  }
-}
-
-void DHT_test1(void *pvParameter) {
-  vTaskDelay(5000 / portTICK_PERIOD_MS);
-
-  printf("Starting DHT Task\n\n");
-
-  struct sensor_msg DHT_3;
-  strcpy(DHT_3.DeviceClass, "08");
-  strcpy(DHT_3.IdDevice, "629X2LJ3");
-  strcpy(DHT_3.MsgType, "02");
-
-  while (1) {
-    float temp = 6.5;
-    float hum = 67;
-    printf("DHT_3: temp: %.2fC humidity: %.2f\n", temp, hum);
-    sprintf(DHT_3.MsgContent, "T%05.2fH%02.2f", temp, hum);
-    xQueueSendToBack(xMQTTDHTQueue, &DHT_3, 0);
-
-    vTaskDelay(2000 / portTICK_PERIOD_MS);
-  }
-}
 
 static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event) {
   esp_mqtt_client_handle_t client = event->client;
@@ -156,42 +83,7 @@ static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event) {
       printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
       printf("DATA=%.*s\r\n", event->data_len, event->data);
 
-      cJSON *json_cmd = cJSON_Parse(event->data);
-      cJSON *json_idmsg = cJSON_GetObjectItem(json_cmd, "IdMsg");
-      char str_idmsg[8];
-      char str_response_status[2] = "2";
-      if (cJSON_IsString(json_idmsg) && (json_idmsg->valuestring != NULL)) {
-        strcpy(str_idmsg, json_idmsg->valuestring);
-        gpio_set_level(2, !gpio_get_level(2));
-        strcpy(str_response_status, "0");
-      } else {
-        printf("error: idmsg is not string or is null");
-        strcpy(str_idmsg, "error");
-      }
-
-      char *str_response_led;
-      cJSON *json_led = cJSON_CreateObject();
-      cJSON_AddStringToObject(json_led, "IdMsg", str_idmsg);
-      cJSON_AddStringToObject(json_led, "Status", str_response_status);
-      str_response_led = cJSON_Print(json_led);
-
-      cJSON_Delete(json_led);
-      cJSON_Delete(json_cmd);
-
-      char cmd_topic[128];
-      char response_topic[128];
-
-      sprintf(cmd_topic, "%.*s", event->topic_len, event->topic);
-      char *pch = strstr(cmd_topic, "cmd/");
-      memcpy(response_topic, cmd_topic, pch - cmd_topic);
-      memcpy(response_topic + (pch - cmd_topic), "cmdAck/", strlen("cmdAck/"));
-      strcpy(response_topic + (pch - cmd_topic) + strlen("cmdAck/"),
-             pch + strlen("cmd/"));
-
-      printf("received: %s\nsent: %s\n", cmd_topic, response_topic);
-
-      esp_mqtt_client_publish(client, response_topic, str_response_led, 0, 1,
-                              0);
+      mqttpoint_receive(client, event);
       break;
     case MQTT_EVENT_ERROR:
       ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
@@ -243,24 +135,7 @@ static void mqtt_app_start(void *pvParameter) {
   esp_mqtt_client_start(client);
 
   while (1) {
-    char *msgbuffer = NULL;
-    char topic[128] = "test/";
-    struct sensor_msg MQTT_MSG;
-
-    xQueueReceive(xMQTTDHTQueue, &MQTT_MSG, portMAX_DELAY);
-
-    cJSON *mqtt_infomsg = cJSON_CreateObject();
-    cJSON_AddStringToObject(mqtt_infomsg, "DeviceClass", MQTT_MSG.DeviceClass);
-    cJSON_AddStringToObject(mqtt_infomsg, "IdDevice", MQTT_MSG.IdDevice);
-    cJSON_AddStringToObject(mqtt_infomsg, "MsgType", MQTT_MSG.MsgType);
-    cJSON_AddStringToObject(mqtt_infomsg, "MsgContent", MQTT_MSG.MsgContent);
-    msgbuffer = cJSON_Print(mqtt_infomsg);
-
-    cJSON_Delete(mqtt_infomsg);
-
-    strcat(topic, MQTT_MSG.IdDevice);
-    esp_mqtt_client_publish(client, topic, msgbuffer, 0, 1, 0);
-    printf("sent message to topic: \"%s\"\n", topic);
+    mqttpoint_send(client);
     vTaskDelay(5000 / portTICK_PERIOD_MS);
   }
 }
