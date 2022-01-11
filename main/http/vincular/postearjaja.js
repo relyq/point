fb9b3eced204f778e90dea3d37d662c2386134bb
usingvar <86341036+usingvar@users.noreply.github.com>
2022-01-10 22:13:42 -0300

    async

--- main/http/vincular/postearjaja.js
+++ main/http/vincular/postearjaja.js
@@ -4,12 +4,12 @@ const gel = (e) => document.getElementById(e);
 
 gel("btnVincular").addEventListener(
     "click",
-    (e) => {
+   async (e) => {
         // si let no funca, pone var
         let codigoID = gel("codigoID").value;
 
         //El connect.json tambien se declara en c, ponele el nombre que mas te guste
-        let test = fetch("/vincular.json", {
+        let test = await fetch("/vincular.json", {
             method: "POST",
             //El X-Codigo-Id lo tenes que declarar en C
             headers: {
