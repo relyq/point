c6c24c38aedcdea48a3fc6f36af475b366011927
relyq <reliqx@gmail.com>
2022-01-10 23:44:45 -0300

    mostrar respuesta de vincular

--- main/http/vincular/postearjaja.js
+++ main/http/vincular/postearjaja.js
@@ -4,12 +4,12 @@ const gel = (e) => document.getElementById(e);
 
 gel("btnVincular").addEventListener(
     "click",
-    (e) => {
+    async(e) => {
         // si let no funca, pone var
         let codigoID = gel("codigoID").value;
 
         //El connect.json tambien se declara en c, ponele el nombre que mas te guste
-        let test = fetch("/vincular.json", {
+        let test = await fetch("/vincular.json", {
             method: "POST",
             //El X-Codigo-Id lo tenes que declarar en C
             headers: {
@@ -19,10 +19,12 @@ gel("btnVincular").addEventListener(
 
             body: { timestamp: Date.now() },
         });
-        let msg = document.getElementsByClassName('message');
+        let data = await test.json();
+        console.log(data);
+        let msg = document.getElementById('message');
         msg.style.display = 'block';
-        msg.textContent  = test['message'];
-        
+        msg.textContent = data['message'];
+
     },
     false
 );
