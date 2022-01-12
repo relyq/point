7e547e856b397fede9e14c34a3119e6b9815d036
relyq <nicoale_mainenti@hotmail.es>
2022-01-11 22:17:26 -0300

    refactoring

---
+++ main/mqttpoint/mqtt_app.c
@@ -0,0 +1,36 @@
+#include "mqttpoint.h"
+
+static const char *TAG = "MQTT_APP";
+
+#if CONFIG_BROKER_CERTIFICATE_OVERRIDDEN == 1
+static const uint8_t mqtt_eclipse_org_pem_start[] =
+    "-----BEGIN CERTIFICATE-----\n" CONFIG_BROKER_CERTIFICATE_OVERRIDE
+    "\n-----END CERTIFICATE-----";
+#else
+extern const uint8_t mqtt_eclipse_org_pem_start[] asm(
+    "_binary_mqtt_eclipse_org_pem_start");
+#endif
+extern const uint8_t mqtt_eclipse_org_pem_end[] asm(
+    "_binary_mqtt_eclipse_org_pem_end");
+
+void mqtt_app_start(void *pvParameter) {
+  ESP_LOGI(TAG, "starting mqtt task");
+
+  const esp_mqtt_client_config_t mqtt_cfg = {
+      .uri = CONFIG_BROKER_URI,
+      .username = "points",
+      .password = "Fm7G7MtV",
+      .keepalive = 60,
+      .cert_pem = (const char *)mqtt_eclipse_org_pem_start,
+  };
+
+  esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+  esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler,
+                                 client);
+  esp_mqtt_client_start(client);
+
+  while (1) {
+    mqttpoint_send(client);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+  }
+}
--- main/mqttpoint/mqttpoint.h
+++ main/mqttpoint/mqttpoint.h
@@ -12,5 +12,9 @@
 void mqttpoint_send(esp_mqtt_client_handle_t client);
 void mqttpoint_receive(esp_mqtt_client_handle_t client,
                        esp_mqtt_event_handle_t event);
+esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event);
+void mqtt_event_handler(void *handler_args, esp_event_base_t base,
+                        int32_t event_id, void *event_data);
+void mqtt_app_start(void *pvParameter);
 
 #endif  // __MQTTPOINT_H__
