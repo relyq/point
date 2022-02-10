75409d0101ab1a33d10b1b38ab69190b02525124
relyq <nicoale_mainenti@hotmail.es>
2022-02-10 01:40:57 -0300

    ota update mqtt receive

--- main/mqttpoint/mqttpoint_receive.c
+++ main/mqttpoint/mqttpoint_receive.c
@@ -1,51 +1,47 @@
 #include "driver/gpio.h"
 #include "mqttpoint.h"
 
-extern void perform_ota_update();
+extern void perform_ota_update(const char *url);
+extern char mac_str[13];
 
 static const char *TAG = "MQTTPOINT_RECEIVE";
 
 void mqttpoint_receive(esp_mqtt_client_handle_t client,
                        esp_mqtt_event_handle_t event) {
-  cJSON *json_cmd = cJSON_Parse(event->data);
-  cJSON *json_idmsg = cJSON_GetObjectItem(json_cmd, "IdMsg");
-  char str_idmsg[8];
-  char str_response_status[2] = "2";
+  cJSON *json_data = cJSON_Parse(event->data);
 
-  if (cJSON_IsString(json_idmsg) && (json_idmsg->valuestring != NULL)) {
-    strcpy(str_idmsg, json_idmsg->valuestring);
-    gpio_set_level(2, !gpio_get_level(2));
-    strcpy(str_response_status, "0");
-  } else {
-    ESP_LOGE(TAG, "IdMsg is not string or is null");
-    strcpy(str_idmsg, "error");
+  // todo esto debería hacerlo de otra forma
+  char topic_base[64] = "test/";
+  strcat(topic_base, mac_str);
+
+  char topic_cmd[64];
+  strcpy(topic_cmd, topic_base);
+  strcat(topic_cmd, "/cmd");
+
+  // la alternativa a strcmp() es un switch con un hash del topico
+  // deberia haber una task que se encargue de manejar los puertos
+  // ahora mismo estoy declarandolo como salida en app_main.c
+  if (!strncmp(event->topic, topic_cmd, event->topic_len)) {
+    if (cJSON_IsNumber(cJSON_GetObjectItem(json_data, "IdMsg"))) {
+      enum idmsg msg = cJSON_GetObjectItem(json_data, "IdMsg")->valueint;
+
+      switch (msg) {
+        case MSG_OUTPUT: {
+          break;
+        }
+        case MSG_UPDATE: {
+          if ((cJSON_IsString(cJSON_GetObjectItem(json_data, "url"))) &&
+              (cJSON_GetObjectItem(json_data, "url")->valuestring != NULL)) {
+            perform_ota_update(
+                cJSON_GetObjectItem(json_data, "url")->valuestring);
+          } else {
+            ESP_LOGE(TAG, "ota url is not a string or is NULL");
+          }
+          break;
+        }
+      }
+    } else {
+      ESP_LOGE(TAG, "IdMsg is not an integer");
+    }
   }
-
-  if (strcmp(event->topic, "test/f008d1d4faf0/update")) {
-    perform_ota_update();
-  }
-
-  /* esto me crashea el dispositivo. de todas formas tengo que cambiarlo
-  char *str_response_led;
-  cJSON *json_led = cJSON_CreateObject();
-  cJSON_AddStringToObject(json_led, "IdMsg", str_idmsg);
-  cJSON_AddStringToObject(json_led, "Status", str_response_status);
-  str_response_led = cJSON_Print(json_led);
-
-  cJSON_Delete(json_led);
-  cJSON_Delete(json_cmd);
-
-  char cmd_topic[128];
-  char response_topic[128];
-
-  // responde al cmdAck del mismo tópico del que llegó el mensaje
-  sprintf(cmd_topic, "%.*s", event->topic_len, event->topic);
-  char *pch = strstr(cmd_topic, "cmd/");
-  memcpy(response_topic, cmd_topic, pch - cmd_topic);
-  memcpy(response_topic + (pch - cmd_topic), "cmdAck/", strlen("cmdAck/"));
-  strcpy(response_topic + (pch - cmd_topic) + strlen("cmdAck/"),
-         pch + strlen("cmd/"));
-
-  esp_mqtt_client_publish(client, response_topic, str_response_led, 0, 1, 0);
-  */
 }
