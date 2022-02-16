e7b86aeae90377491b056cbdcbe1883dbdb4242b
relyq <nicoale_mainenti@hotmail.es>
2022-02-16 20:35:20 -0300

    queue en .c y err

--- main/sensors/DHT_task.c
+++ main/sensors/DHT_task.c
@@ -3,6 +3,8 @@
 
 static const char* TAG = "DHT";
 
+extern QueueHandle_t xMQTTDHTQueue;
+
 void DHT_task(void* pvParameter) {
   static const dht_sensor_type_t sensor_type = DHT_TYPE_DHT11;
   static const gpio_num_t dht_gpio = 4;
@@ -10,18 +12,21 @@ void DHT_task(void* pvParameter) {
 
   esp_err_t err;
 
-  strcpy(DHT.DeviceClass, "08");
+  // todo esto podria ser una funcion que me inicialice el struct
+  strcpy(DHT.DeviceClass, "08");  // deberia ser un enum
   strcpy(DHT.IdDevice, mac_str);
   strcpy(DHT.MsgType, "02");
 
   while (1) {
     float temp;
     float hum;
-    if (dht_read_float_data(sensor_type, dht_gpio, &hum, &temp) == ESP_OK) {
-      sprintf(DHT.MsgContent, "T%05.2fH%02.2f", temp, hum);
+
+    err = dht_read_float_data(sensor_type, dht_gpio, &hum, &temp);
+    if (err == ESP_OK) {
+      snprintf(DHT.MsgContent, 64, "T%05.2fH%02.2f", temp, hum);
       xQueueSendToBack(xMQTTDHTQueue, &DHT, 0);
     } else {
-      ESP_LOGE(TAG, "could not read data from sensor");
+      ESP_LOGE(TAG, "could not read data from sensor: %d", err);
     }
     vTaskDelay(2000 / portTICK_PERIOD_MS);
   }
