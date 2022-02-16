b69a4c082d017323fbd0d00ff076a9e3ae752516
relyq <nicoale_mainenti@hotmail.es>
2022-02-16 20:12:23 -0300

    DHT_1>DHT

--- main/sensors/DHT_task.c
+++ main/sensors/DHT_task.c
@@ -8,7 +8,7 @@ void DHT_task(void* pvParameter) {
 
   static const dht_sensor_type_t sensor_type = DHT_TYPE_DHT11;
   static const gpio_num_t dht_gpio = 4;
-  struct sensor_msg DHT_1;
+  struct sensor_msg DHT;
 
   char str_mac[13];
   esp_err_t err;
@@ -22,16 +22,16 @@ void DHT_task(void* pvParameter) {
     ESP_LOGE(TAG, "esp_efuse_mac_get_default: %s", esp_err_to_name(err));
   }
 
-  strcpy(DHT_1.DeviceClass, "08");
-  strcpy(DHT_1.IdDevice, str_mac);
-  strcpy(DHT_1.MsgType, "02");
+  strcpy(DHT.DeviceClass, "08");
+  strcpy(DHT.IdDevice, str_mac);
+  strcpy(DHT.MsgType, "02");
 
   while (1) {
     float temp;
     float hum;
     if (dht_read_float_data(sensor_type, dht_gpio, &hum, &temp) == ESP_OK) {
-      sprintf(DHT_1.MsgContent, "T%05.2fH%02.2f", temp, hum);
-      xQueueSendToBack(xMQTTDHTQueue, &DHT_1, 0);
+      sprintf(DHT.MsgContent, "T%05.2fH%02.2f", temp, hum);
+      xQueueSendToBack(xMQTTDHTQueue, &DHT, 0);
     } else {
       ESP_LOGE(TAG, "could not read data from sensor");
     }
